621087e018209938b87fb30548de96524fe65703
Dance Maniac <gazelev6@yandex.ru>
2022-12-17 10:36:33 +0300

    CGIM2 Addon

---
+++ CGIM2 Addon/gamedata/shaders/r1/effects_waterstuden.s
@@ -0,0 +1,16 @@
+local tex_base                = "water\\water_studen"
+local tex_env                = "sky\\sky_5_cube"
+local tex_dist                = "water\\water_dudv"
+local tex_dist2                = "water\\water_dudv"
+
+function normal                (shader, t_base, t_second, t_detail)
+  shader:begin                ("water","water")
+        : sorting        (2, true)
+        : blend                (true,blend.srcalpha,blend.invsrcalpha)
+              : aref                (true,0)
+              : zb                (true,false)
+              : distort        (true)
+              : fog                (true)
+  shader:sampler        ("s_base")       :texture  (tex_base)
+  shader:sampler        ("s_env")        :texture  (tex_env)   : clamp()
+end
